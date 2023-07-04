15ef815aee6101255900a9e3b442d5c21dca062b
amaniesseili <esseiliamani@hotmail.com>
2023-07-04 16:27:30 +0200

    n. mult. x 3 e 5 con scritta fizz e buzz sul cons.

---
+++ main.js
@@ -0,0 +1,17 @@
+for (let i=0; i<=100;i++){
+    if(i % 3 === 0){
+        console.log("fizz")
+    } else if (i % 5 === 0 ){
+        console.log("Buzz")
+    }else{
+        console.log(i)
+    }
+}
+
+// for è da 0 a 100, dentro il ciclo viene 
+// effettuato un controllo per verificare 
+// se il numero e multiplicato per 3 e 5  usando il modulo %
+// se e multiplicato per 3 viene stampato fizz
+//  se e multiplicato per 5 viene stampato buzz 
+//  altrimenti si stampa i numero (i)
+
